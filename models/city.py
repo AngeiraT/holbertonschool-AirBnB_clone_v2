c2c282a2e4975b8d8d27e7ece6e76ba357b971ea
AngeiraT <thaispur04@icloud.com>
2022-11-23 16:03:16 -0400

    edited basemodel import order

--- models/city.py
+++ models/city.py
@@ -2,7 +2,7 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
-from models.base_model import Base, BaseModel
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
