413bd40f598b55c4b7feabc2174b68eda7436b22
AngeiraT <thaispur04@icloud.com>
2022-11-25 20:27:45 -0400

    added var enviro

--- models/amenity.py
+++ models/amenity.py
@@ -3,14 +3,21 @@
 from sqlalchemy.orm import relationship
 from models.base_model import Base, BaseModel
 from sqlalchemy import Column, String, ForeignKey
-
+from os import getenv
 
 class Amenity(BaseModel, Base):
     """
     Amenity inherits from BaseModel and Base (respect the order)
     """
-    __tablename__ = 'amenities'
-    name = Column(String(128), nullable=False)
-    places_amenities = relationship(
-        "Place",
-        secondary="place_amenity")
+    
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        class Amenity(BaseModel, Base):
+            __tablename__ = 'amenities'
+            name = Column(String(128), nullable=False)
+            places_amenities = relationship(
+            "Place",
+            secondary="place_amenity")
+    else:
+        class Amenity(BaseModel);
+        """Create class Amenity"""
+        name = ""
