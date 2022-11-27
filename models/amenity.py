47414f6d745fb2da6870fa4516130e2bd3785c16
AngeiraT <thaispur04@icloud.com>
2022-11-27 16:23:27 -0400

    added init to each model

--- models/amenity.py
+++ models/amenity.py
@@ -11,13 +11,15 @@ class Amenity(BaseModel, Base):
     """
     Amenity inherits from BaseModel and Base (respect the order)
     """
-    __tablename__ = 'amenities'
     if getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = 'amenities'
         name = Column(String(128), nullable=False)
         places_amenities = relationship(
             "Place",secondary="place_amenity")
     else:
-        class Amenity(BaseModel):
-            """Create class Amenity"""
-            name = ""
-            state_id = ""
+        name = ""
+        state_id = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes Amenity"""
+        super().__init__(*args, **kwargs)
