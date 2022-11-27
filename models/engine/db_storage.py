2515b2d5d7261a906cade781cfbd0454b36cf4be
AngeiraT <thaispur04@icloud.com>
2022-11-27 17:05:20 -0400

    eliminated imports in db storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -73,14 +73,6 @@ class DBStorage:
 
     def reload(self):
         """ Reload all tables and session from the engine """
-        from models.base_model import BaseModel, Base
-        from models.amenity import Amenity
-        from models.city import City
-        from models.place import Place
-        from models.review import Review
-        from models.state import State
-        from models.user import User
-        
         Base.metadata.create_all(self.__engine)
         
         session_factory = sessionmaker(
