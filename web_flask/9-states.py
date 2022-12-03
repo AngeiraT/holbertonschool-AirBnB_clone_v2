2fe03bb617a8054531f2e21e093fbda061f20669
AngeiraT <thaispur04@icloud.com>
2022-12-03 12:16:03 -0400

    edited global comment in file 9-states

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -4,7 +4,10 @@
 from flask import Flask, render_template
 from models import storage
 from models import State
+
+
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
@@ -13,14 +16,14 @@ def teardown(exception):
     storage.close()
 
 
-@app.route('/states/', strict_slashes=False)
+@app.route('/states/')
 def no_cities():
     """states only"""
     states = storage.all('State')
     return render_template('9-states.html', states=states)
 
 
-@app.route('/states/<id>', strict_slashes=False)
+@app.route('/states/<id>')
 def state_and_cities(id):
     """by id"""
     for state in storage.all("State").values():
