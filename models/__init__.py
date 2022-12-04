2b5c6d5814ba7ab0b843b24b87deb2b2626308f3
spcruz5 <spcruz5@gmail.com>
2022-12-04 11:57:43 -0500

    Adding init

--- models/__init__.py
+++ models/__init__.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
-from models.engine.db_storage import DBStorage
-import os
+from os import getenv
 
-HBNB_TYPE_STORAGE = os.getenv("HBNB_TYPE_STORAGE")
-
-if HBNB_TYPE_STORAGE == "db":
+HBNB_TYPE_STORAGE = getenv('HBNB_TYPE_STORAGE')
+    
+if HBNB_TYPE_STORAGE == 'db':
+    from models.engine.db_storage import DBStorage
     storage = DBStorage()
-    storage.reload()
 else:
+    from models.engine.file_storage import FileStorage
     storage = FileStorage()
-    storage.reload()
+storage.reload()
