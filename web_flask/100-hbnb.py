8efb11cb132b29be7e4b6c246d7b8148e4bb8fba
AngeiraT <thaispur04@icloud.com>
2022-12-04 22:36:34 -0400

    eliminate debug

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -18,9 +18,9 @@ def teardown(exception):
 @app.route('/hbnb', strict_slashes=False)
 def hbnb_filters():
     """ /hbnb route """
-    states = storage.all(State)
-    amenities = storage.all(Amenity)
-    places = storage.all(Place)
+    states = storage.all(State).values()
+    amenities = storage.all(Amenity).values()
+    places = storage.all(Place).values()
     return render_template('100-hbnb.html', states=states, amenities=amenities, places=places)
 
 
--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -3,6 +3,8 @@
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+
+
 app = Flask(__name__)
 
 
