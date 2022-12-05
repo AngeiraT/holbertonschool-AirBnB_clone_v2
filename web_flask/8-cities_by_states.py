115cc200fbaec4a5d350e48a84079e8c202a3101
AngeiraT <thaispur04@icloud.com>
2022-12-04 21:17:26 -0400

    added import state

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -4,6 +4,7 @@ Script that starts a Flask web application
 """
 from flask import Flask, render_template
 from models import storage
+from models.state import State
 
 
 app = Flask(__name__)
@@ -12,7 +13,7 @@ app = Flask(__name__)
 @app.route('/cities_by_states', strict_slashes=False)
 def states_list():
     """ State list """
-    states = storage.all("State")
+    states = storage.all(State)
     return render_template("8-cities_by_states.html", states=states)
 
 
