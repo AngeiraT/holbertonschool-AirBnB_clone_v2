57a821161e2c888f617e7bba3023125a5a992dd5
spcruz5 <spcruz5@gmail.com>
2022-12-07 12:36:47 -0500

    Revert "Removing is statement for place ammeni"
    This reverts commit 1523d49b5f00dc48f3f4915133a6d3705370ce75.

--- models/place.py
+++ models/place.py
@@ -32,41 +32,59 @@ class Place(BaseModel, Base):
         """ A place to stay """
 
         __tablename__ = "places"
+        if getenv("HBNB_TYPE_STORAGE") == "db":
+            city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+            user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+            name = Column(String(128), nullable=False)
+            description = Column(String(1024))
+            number_rooms = Column(Integer, nullable=False, default=0)
+            number_bathrooms = Column(Integer, nullable=False, default=0)
+            max_guest = Column(Integer, nullable=False, default=0)
+            price_by_night = Column(Integer, nullable=False, default=0)
+            latitude = Column(Float)
+            longitude = Column(Float)
+            amenity_ids = []
         
-        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-        name = Column(String(128), nullable=False)
-        description = Column(String(1024))
-        number_rooms = Column(Integer, nullable=False, default=0)
-        number_bathrooms = Column(Integer, nullable=False, default=0)
-        max_guest = Column(Integer, nullable=False, default=0)
-        price_by_night = Column(Integer, nullable=False, default=0)
-        latitude = Column(Float)
-        longitude = Column(Float)
-        amenity_ids = []
-        
-        amenities = relationship("Amenity", secondary=place_amenity,
-                             viewonly=False)
+            amenities = relationship("Amenity", secondary=place_amenity,
+                                 viewonly=False)
 
-        reviews = relationship(
-            'Review',backref='state',cascade="all, delete, delete-orphan")
+            reviews = relationship(
+                'Review',backref='state',cascade="all, delete, delete-orphan")
 
-        @property
-        def reviews(self):
-            """ Getter att that returns list of Reviews instances """
-            instances = models.storage.all(Review)
-            new = []
-            for review in instances.values():
-                if review.place_id == (self.id):
-                    new.append(review)
-            return new
+        else:
+            city_id = ""
+            user_id = ""
+            name = ""
+            description = ""
+            number_rooms = 0
+            number_bathrooms = 0
+            max_guest = 0
+            price_by_night = 0
+            latitude = 0.0
+            longitude = 0.0
+            amenity_ids = []
 
-        @reviews.setter
-        def amenities(self, obj):
-            """
-            Setter attribute amenities that handles append method
-            for adding an Amenity.id to the attribute amenity_ids.
-            """
-            from models.amenity import Amenity
-            if isinstance(obj, Amenity):
-                self.amenity_ids.append(obj.id)
+            @property
+            def reviews(self):
+                """ Getter att that returns list of Reviews instances """
+                instances = models.storage.all(Review)
+                new = []
+                for review in instances.values():
+                    if review.place_id == (self.id):
+                        new.append(review)
+                return new
+
+            @reviews.setter
+            def amenities(self, obj):
+                """
+                Setter attribute amenities that handles append method
+                for adding an Amenity.id to the attribute amenity_ids.
+                """
+                from models.amenity import Amenity
+                if isinstance(obj, Amenity):
+                    self.amenity_ids.append(obj.id)
+
+                    
+        def __init__(self, *args, **kwargs):
+            """initializes Amenity"""
+            super().__init__(*args, **kwargs)
