f5ee88405467ab9150d75c45a64bef4f0754ce5b
spcruz5 <spcruz5@gmail.com>
2022-12-07 12:36:52 -0500

    Revert "REmoving save() at attributes"
    This reverts commit a1e41f3e6c1cb5892b71a170b68ca4d6a77ac441.

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -2,13 +2,12 @@
 """Storage of MySQL"""
 from models.base_model import BaseModel
 from models.base_model import Base
-from sqlalchemy import MetaData, create_engine
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import scoped_session
 from os import getenv
 from models.state import State
 from models.city import City
-from sqlalchemy.orm import Session
 
 
 class DBStorage:
@@ -29,7 +28,6 @@ class DBStorage:
                                               HBNB_MYSQL_HOST,
                                               HBNB_MYSQL_DB),
                                       pool_pre_ping=True)
-        metadata = MetaData()
         if getenv('HBNB_ENV') == 'test':
             Base.metadata.drop_all(self.__engine)
 
